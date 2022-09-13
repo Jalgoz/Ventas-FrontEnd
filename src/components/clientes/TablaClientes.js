a249dba5c94224b22b78439f202c53900362351e
Jalgoz <jalgoz95@gmail.com>
2022-09-13 12:52:28 -0400

    Se termino la tabla de usuario sin conexión a la base de datos

--- src/components/clientes/TablaClientes.js
+++ src/components/clientes/TablaClientes.js
@@ -1,4 +1,25 @@
+import { useState } from "react";
+
 const TablaClientes = () => {
+	const [listaClientes, setListaClientes] = useState([
+		{
+			idCliente: 0,
+			nombre: "Seth",
+			apellidos: "Lozada Gómez",
+			direccion: "Irpavi",
+			telefono: "",
+			email: "sethsillo@gmail.com",
+		},
+		{
+			idCliente: 1,
+			nombre: "Isis",
+			apellidos: "Lozada Gómez",
+			direccion: "Irpavi",
+			telefono: "",
+			email: "isis00@gmail.com",
+		},
+	]);
+
 	return (
 		<div className="table-container">
 			<table className="table is-hoverable is-fullwidth">
@@ -8,32 +29,34 @@ const TablaClientes = () => {
 						<th>Nombres</th>
 						<th>Apellidos</th>
 						<th>Dirección</th>
-						<th>Telefono</th>
+						<th>"Telefono"</th>
 						<th>Email</th>
 					</tr>
 				</thead>
 
 				<tbody>
-					<tr>
-						<td>
-							<button className="button is-info mr-1" title="Modificar">
-								<span className="icon is-small">
-									<i className="fas fa-edit"></i>
-								</span>
-							</button>
+					{listaClientes.map(cliente => (
+						<tr key={cliente.idCliente}>
+							<td>
+								<button className="button is-info mr-1" title="Modificar">
+									<span className="icon is-small">
+										<i className="fas fa-edit"></i>
+									</span>
+								</button>
 
-							<button className="button is-danger" title="Eliminar">
-								<span className="icon is-small">
-									<i className="fas fa-trash-alt"></i>
-								</span>
-							</button>
-						</td>
-						<td>Freddy</td>
-						<td>Aguilar</td>
-						<td>C. Lincon</td>
-						<td>73097333</td>
-						<td>freddy95@gmail.com</td>
-					</tr>
+								<button className="button is-danger" title="Eliminar">
+									<span className="icon is-small">
+										<i className="fas fa-trash-alt"></i>
+									</span>
+								</button>
+							</td>
+							<td>{cliente.nombre}</td>
+							<td>{cliente.apellidos}</td>
+							<td>{cliente.direccion}</td>
+							<td>{cliente.telefono}</td>
+							<td>{cliente.email}</td>
+						</tr>
+					))}
 				</tbody>
 			</table>
 		</div>
